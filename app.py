bc40fdfd1537f74038a9ecdc75d604ce00efdf30
Ketro Sithole <101246585+KetroSithole@users.noreply.github.com>
2023-07-15 00:03:41 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title("Hello, Binder!")
+    st.write("This is a simple Streamlit app hosted on Binder.")
+    st.write("Welcome to the world of interactive web applications!")
+
+if __name__ == '__main__':
+    main()
